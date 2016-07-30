7b2b5cf23cd20b39e7cae7045659e750ca63a137
E1k3 <e_geba02@wwu.de>
2016-07-31 01:42:55 +0200

    Added sync script

---
+++ syncdotfiles.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+dfdirectory="/home/eike/fakedotfiles"
+homedirectory="/home/eike/fakehome"
+backupdirectory="/home/eike/fakebackups"
+
+[ "$dfdirectory" == "$homedirectory" ] && echo "ERROR: same directory" && exit
+
+files="$(find "$dfdirectory" -type f)"
+
+echo "The following files will be linked to their respective folders:"
+printf "$files \n"
+
+[ "$1" == "-y" ] || (echo -n "Continue? (n)" && read yn)
+[ "$yn" == "n" ] && exit
+
+for filename in $files; do
+	relpath="$(realpath --relative-to="$dfdirectory" "$filename")"
+
+	if [ -e "${homedirectory}/$relpath" ]; then
+
+		echo "${homedirectory}/$relpath"
+		echo "This file already exists."
+		[ "$1" == "-y" ] || (echo -n "Continue? (n)" && read yn)
+		[ "$yn" == "n" ] && exit
+
+		mv "${homedirectory}/$relpath" "${backupdirectory}/$relpath"
+	fi
+
+	ln -s "${dfdirectory}/$relpath" "${homedirectory}/$relpath"
+done
