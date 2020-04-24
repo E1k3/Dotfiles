d525068fb093447f76201a7cf48b1d4d52b5fb49
Eike Gebauer <e_geba02@wwu.de>
2020-04-24 11:56:21 +0200

    Moved volume script back into i3 config after simplification using flock.

--- files/.config/i3/volume.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# Script to prevent concurrent calls to amixer that mess up volume settings
-
-[ ! -e "volume.lock" ] && touch volume.lock && amixer -q -D pulse sset Master $1 && pkill -RTMIN+10 i3blocks && rm -rf volume.lock
-
